9e319ff0e2cff1964c181ac950a2b5a72ebab464
jiangshulun <jiangshulun@joyme.sg>
2023-08-02 17:59:46 +0800

    update

--- JRSubDemo/JRSubDemo/JRCnmTool.swift
+++ JRSubDemo/JRSubDemo/JRCnmTool.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public class JRCnmTool {
     
-    public func setupFKUM() {
+    static public func setupFKUM() {
         UMConfigure.initWithAppkey("64ca189cbd4b621232e5d486", channel: "App Store")
         UMConfigure.setLogEnabled(true)
     }
