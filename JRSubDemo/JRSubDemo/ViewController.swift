13beecde29e17ee7eff661bdaa68b78f5c67d215
jiangshulun <jiangshulun@joyme.sg>
2023-08-02 14:41:11 +0800

    更新代码

--- JRSubDemo/JRSubDemo/ViewController.swift
+++ JRSubDemo/JRSubDemo/ViewController.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let fuck = JRFuckTool()
+        fuck.caonima()
+        
+        let fuckView = UIView(frame: CGRectZero)
+        fuckView.backgroundColor = UIColor.red
+        self.view.addSubview(fuckView)
+        
+        fuckView.snp.makeConstraints { make in
+            make.center.equalTo(self.view)
+            make.size.equalTo(CGSizeMake(100, 100))
+        }
     }
 
 
