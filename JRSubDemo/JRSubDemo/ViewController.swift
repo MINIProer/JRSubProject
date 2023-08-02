0c80035ebf56796a9780cc728ef4484ca73350cf
jiangshulun <jiangshulun@joyme.sg>
2023-08-02 10:47:36 +0800

    创建项目子工程文件

---
+++ JRSubDemo/JRSubDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JRSubDemo
+//
+//  Created by jiangshulun on 2023/8/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
